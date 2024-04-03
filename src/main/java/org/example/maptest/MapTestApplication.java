169924b67747799c48c64d51cd76f8c3a572d87e
mabyoungg <mabyoungg@gmail.com>
2024-04-03 18:30:57 +0900

    init: 프로젝트 생성

---
+++ src/main/java/org/example/maptest/MapTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.maptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/maptest/MapTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.maptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
