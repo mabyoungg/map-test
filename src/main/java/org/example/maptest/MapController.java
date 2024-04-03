58dc5e0bd503596bebcc2072c0fc073d556e3e3c
mabyoungg <mabyoungg@gmail.com>
2024-04-03 18:33:05 +0900

    feat: 네이버 지도 추가

---
+++ src/main/java/org/example/maptest/MapController.java
@@ -0,0 +1,12 @@
+package org.example.maptest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MapController {
+    @GetMapping("/naver-map")
+    public String getNaverMap() {
+        return "naverMap";
+    }
+}
